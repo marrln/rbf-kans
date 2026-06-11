a335b23a52a8b68a2b4f33010c0639bc2f5f1a7b
Maria Eleni Patsi <mrlnpts@outlook.com.gr>
2026-06-11 12:01:40 +0300

    Remove old commented out code and debugs, rename variables and edit code for looking more clean. Add comments about future fixes

--- train_model.py
+++ train_model.py
@@ -79,20 +79,15 @@ if __name__ == '__main__':
     model_config = load_config(args.model_config, locals=get_locals())
     set_seed(train_config['seed'])
 
-    # Instantiate models
-    model = instantiate(model_config, 'model')
-    print('-- Model :', model)
-
-    # Instantiate criterion
+    # Instantiate model, criterion, optimizer, scheduler
+    model     = instantiate(model_config, 'model')
     criterion = instantiate(train_config, 'criterion')
-    print('-- Criterion :', criterion)
-
-    # Instantiate optimizer
     optimizer = instantiate(train_config, 'optimizer', model.parameters(), lr=train_config['lr'])
-    print('-- Optimizer :', optimizer)
-
-    # Instantiate scheduler
     scheduler = instantiate(train_config, 'scheduler', optimizer)
+    
+    print('-- Model :',     model)
+    print('-- Criterion :', criterion)
+    print('-- Optimizer :', optimizer)
     print('-- Scheduler :', scheduler)
 
     # Instantiate evaluation criteria
@@ -102,7 +97,7 @@ if __name__ == '__main__':
         for key, val in eval_criteria.items():
             print('  --', key, ':', val)
     else:
-        print('  No evaluation criteria.')
+        print('  No evaluation criteria specified.')
         
     # Instantiate callbacks
     callbacks = weak_instantiate_all(train_config['callbacks'])
@@ -110,9 +105,10 @@ if __name__ == '__main__':
 
     build_dataset() # Build dataset if not already built
     
-    data, labels = get_dataset('train_val')
+    # BUG: we apply data augmentation on the CPU plus on the validation set    
     preprocess_data = A.Compose([
             # NOTE: Here you can edit the data augmentation pipeline as needed
+            # BUG: Need dedicated callbacks 
             # A.HorizontalFlip(p=train_config['probability']),
             # A.VerticalFlip(p=train_config['probability']),
             # A.ShiftScaleRotate(scale_limit=(-0.2, 0), rotate_limit=(-90, 90), p=train_config['probability']),
@@ -122,47 +118,42 @@ if __name__ == '__main__':
             A.Normalize(normalization='min_max_per_channel'),
             A.ToTensorV2(),
         ],
-        # telemetry=False,
         seed=train_config['seed'],
     )
     
+    data, labels = get_dataset('train_val')
     # Create iterable train and val datasets
-    train_loader, val_loader = smart_split_dataset(
-        splits=train_config['splits'],
-        full_dataset=None,
-        groups=get_groups(),
-        seed=train_config['seed']
+    train_indices, val_indices = smart_split_dataset(
+        splits       = train_config['splits'],
+        full_dataset = None,
+        groups       = get_groups(),
+        seed         = train_config['seed']
     )
-    print(f"DEBUG: train indices count = {len(train_loader)}")
-    print(f"DEBUG: val indices count = {len(val_loader)}")
-    print(f"DEBUG: type(train_idx) = {type(train_loader)}")
-    print(f"DEBUG: first few train_idx = {train_loader[:5]}")
     
-    train_loader = GenericDataset(
-        data[train_loader], labels[train_loader],
-        task=train_config['task'],
-        return_weights=train_config['sample_weight'],
-        # preprocess_data=lambda **kwargs: preprocess_data(**kwargs)[list(kwargs.keys())[0]],
-        preprocess_data         = lambda x: preprocess_data(image=x)["image"],
-        preprocess_targ=None,
-        flatten=model_config['flatten'],
+    train_dataset = GenericDataset(
+        data            = data[train_indices], 
+        labels          = labels[train_indices],
+        task            = train_config['task'],
+        return_key      = False,
+        return_weights  = train_config['sample_weight'],
+        preprocess_data = lambda x: preprocess_data(image=x)["image"],
+        preprocess_targ = None,
+        flatten         = model_config['flatten'],
     )
-    print(f"DEBUG: train_loader length = {len(train_loader)}")
-    print(f"DEBUG: train_loader data shape = {train_loader.data.shape if hasattr(train_loader.data, 'shape') else len(train_loader.data)}")
-    
-    val_loader = GenericDataset(
-        data[val_loader], labels[val_loader],
-        task=train_config['task'],
-        return_weights=train_config['sample_weight'],
-        # preprocess_data=lambda **kwargs: preprocess_data(**kwargs)[list(kwargs.keys())[0]],
-        preprocess_data         = lambda x: preprocess_data(image=x)["image"],
-        preprocess_targ=None,
-        flatten=model_config['flatten'],
+
+    val_dataset = GenericDataset(
+        data            = data[val_indices], 
+        labels          = labels[val_indices],
+        task            = train_config['task'],
+        return_key      = False,
+        return_weights  = train_config['sample_weight'],
+        preprocess_data = lambda x: preprocess_data(image=x)["image"],
+        preprocess_targ = None,
+        flatten         = model_config['flatten'],
     )
     
     os.makedirs(os.path.join(args.test_dir, 'models'), exist_ok=True)
-    # data_sample = next(iter(train_loader))[0] BUG: this does not work because train_loader is not a DataLoader yet, but a Dataset. We need to get a sample directly from the dataset.
-    data_sample = train_loader[0][0]
+    data_sample = train_dataset[0][0]
     print(
         '-- Model Summary :',
         get_summary(
@@ -174,43 +165,44 @@ if __name__ == '__main__':
     )
 
     train_loader = DataLoader(
-        train_loader,
-        batch_size=train_config['batch_size'],
-        num_workers=os.cpu_count(),
-        persistent_workers=True,
-        pin_memory=device == torch.device('cuda'),
+        dataset            = train_dataset,
+        batch_size         = train_config['batch_size'],
+        num_workers        = os.cpu_count(),
+        persistent_workers = True,
+        pin_memory         = (device == torch.device('cuda')),
     )
     val_loader = DataLoader(
-        val_loader,
-        batch_size=train_config['batch_size'],
-        num_workers=os.cpu_count(),
-        pin_memory=device == torch.device('cuda'),
+        dataset            = val_dataset,
+        batch_size         = train_config['batch_size'],
+        num_workers        = os.cpu_count(),
+        persistent_workers = True,
+        pin_memory         = (device == torch.device('cuda')),
     )
     
     print('-- Using dataset split :', train_config['splits'])
     print('  -- Train      :', len(train_loader.dataset))
     print('  -- Validation :', len(val_loader.dataset))
-    print('-- Sample weight:', train_config['sample_weight'])
+    print('  -- Using Sample weight:', train_config['sample_weight'])
     
     history = train(
-        model,
-        train_dataloader=train_loader,
-        eval_dataloader=val_loader,
-        criterion=criterion,
-        eval_criteria=eval_criteria,
-        optimizer=optimizer,
-        scheduler=scheduler,
-        epochs=train_config['epochs'],
-        patience=train_config['patience'],
-        update_limit=100,
-        sample_weight=train_config['sample_weight'],
-        clip_limit=0.5,
-        top_dirname=args.test_dir,
-        device=device,
-        evaluate_training=False,
-        saving_steps='log',
-        show_pbar=None if args.no_pbar else 'external',
-        callbacks=callbacks,
-        callbacks_arguments=callbacks_arguments,
+        model               = model,
+        train_dataloader    = train_loader,
+        eval_dataloader     = val_loader,
+        criterion           = criterion,
+        eval_criteria       = eval_criteria,
+        optimizer           = optimizer,
+        scheduler           = scheduler,
+        epochs              = train_config['epochs'],
+        patience            = train_config['patience'],
+        update_limit        = 100,
+        sample_weight       = train_config['sample_weight'],
+        clip_limit          = 1.0, # NOTE: / BUG: Grad clipping is hardcoded
+        top_dirname         = args.test_dir,
+        device              = device,
+        evaluate_training   = False,
+        saving_steps        = 'log',
+        show_pbar           = None if args.no_pbar else 'external',
+        callbacks           = callbacks,
+        callbacks_arguments = callbacks_arguments,
     )
     print('-- Model :', model)
